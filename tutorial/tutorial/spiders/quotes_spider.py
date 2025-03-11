1fdf4e9b61d809f6762d8813c080d2f95f7c7d9d
Mel Seleghin <melseleghin@gmail.com>
2025-03-11 08:46:54 -0300

    inserindo funcionalidade para gerar um arquivo CSV com todas as marcas coletadas.

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import csv
 from pymongo import MongoClient
 
 MONGO_URI = "mongodb+srv://melseleghin:Morangu!nh0@brands.tr5lj.mongodb.net"
@@ -7,6 +8,7 @@ COLLECTION_DATA = "brands"
 
 client = MongoClient(MONGO_URI)
 db = client[DATABASE_NAME]
+
 
 class BrandsSpider(scrapy.Spider):
     name = "brands"
@@ -18,19 +20,24 @@ class BrandsSpider(scrapy.Spider):
     def parse(self, response):
         data_to_insert = []
         for brand in response.css("div#ctl00_mainContent_brandPanel div.brandLine"):
-            data_to_insert.append({"Brand": brand.css("span.rankingName::text").get()})
-            self.all_brands.append(brand.css("span.rankingName::text").get())
-            yield {
-                "Brand": brand.css("span.rankingName::text").get()
-            }
+            brand_name = brand.css("span.rankingName::text").get()
+            if brand_name:
+                data_to_insert.append({"Brand": brand_name})
+                self.all_brands.append(brand_name)
+                yield {"Brand": brand_name}
 
         # Coleta os links das letras (A-Z)
         next_letters = response.css("div#ctl00_mainContent_filterPanel a::attr(href)").getall()
         for letter_link in next_letters:
             yield response.follow(letter_link, callback=self.parse)
 
-        # db[COLLECTION_DATA].insert_many(data_to_insert)
-
     def closed(self, reason):
         if self.all_brands:
             db[COLLECTION_DATA].insert_one({"All_Brands": self.all_brands})
+
+            # Gerando arquivo CSV
+            with open("brands.csv", mode="w", newline="", encoding="utf-8") as file:
+                writer = csv.writer(file)
+                writer.writerow(["Brand"])
+                for brand in self.all_brands:
+                    writer.writerow([brand])
